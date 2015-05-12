1edc7c8136c3cae9b653c3322ffebe08dba38313
Chris Wong <chris@cogifire.com>
2015-05-11 20:46:32 -0700

    Base code
    Added: Project files + main.cpp with default template.

---
+++ teamx/main.cpp
@@ -0,0 +1,56 @@
+/*
+Program Description: Write a detailed description of WHAT this program does from a user's perspective.
+It's extremely imortant that you can provide a detailed and relevant description in this space.
+
+Written By: FirstName LastName
+
+Date Submitted: mm/dd/yyyy
+
+IDE Used: Xcode / Visual Studio Express 2013 / Code::Blocks / ...
+*/
+// All include statements starting from here
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// All global constants starting from here
+
+// All function declaration starting from here
+
+// It is extremely important that you document each function declaration
+// with the required elements: purpose, preconditions, parameters, postconditions
+// and returns.
+
+/**
+* Display a welcome message at the beginning of the program.
+* @pre none
+* @post A welcome message has been displayed.
+*/
+void hello();
+
+/**
+* Display a farewell message at the end of the program.
+* @pre none
+* @post A farewell message has been displayed.
+*/
+void goodBye();
+
+// Main function
+int main() {
+	hello();
+
+	// Attention:
+	// Provide data here and call your functions
+	// The main function should be simple and straightforward. 
+	// There should not be any 'logic' in the main function at all.
+
+	goodBye();
+	return 0;
+}
+
+// All function definitions starting from here
+
+/*
+Copy program interaction between user and the program here.
+*/
